f484837e8d2417b8f81ff92bf6c47f8c6f4c7b90
Camilo <trresqwe@gmail.com>
2021-01-28 11:22:55 -0500

    Update files

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,3 +9,17 @@ from models import storage
 def Status():
     """Status"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False, methods=['GET'])
+def Stats():
+    """Stats views"""
+    return jsonify(
+        {
+            'amenities': storage.count("Amenity"),
+            'cities': storage.count("City"),
+            'places': storage.count("Place"),
+            'reviews': storage.count("Review"),
+            'states': storage.count("State"),
+            'users': storage.count("User")
+        })
