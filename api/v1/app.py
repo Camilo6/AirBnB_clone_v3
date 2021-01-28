1be93351a3c59c7900640baad1bd5bdedfc665ad
Camilo <trresqwe@gmail.com>
2021-01-27 19:47:45 -0500

    Update

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 API file
 """
 
-from flask import Flask
+from flask import Flask, Blueprint
 from models import storage
 from api.v1.views import app_views
 
@@ -12,12 +12,11 @@ app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def teardown(exception):
+def teardown_appcontext(exception):
     """close the storage on teardown"""
     storage.close()
 
 
 if __name__ == "__main__":
     app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=int(os.getenv('HBNB_API_PORT', '5000')),
-            threaded=True, debug=True)
+            port=int(os.getenv('HBNB_API_PORT', '5000')))
