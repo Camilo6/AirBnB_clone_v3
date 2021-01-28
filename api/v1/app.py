fbaac7edc5d45e6a707b7bca0f03b817fd7c320e
Camilo <trresqwe@gmail.com>
2021-01-28 12:01:08 -0500

    Update app

--- api/v1/app.py
+++ api/v1/app.py
@@ -23,7 +23,7 @@ def teardown_appcontext(exception):
 @app.errorhandler(404)
 def not_found(error):
     """handler error 404"""
-    return jsonify({'error': 'Not found'}), 404
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
