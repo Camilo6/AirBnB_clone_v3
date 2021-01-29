ad2532a66d8630a413bebd2d3f28d97456f5c03e
afarizap <afarizap@gmail.com>
2021-01-28 23:24:57 -0500

    update cities with create city module

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -4,7 +4,7 @@ from api.v1.views import app_views
 from models import storage
 from models.state import State
 from models.city import City
-from flask import jsonify, abort
+from flask import jsonify, abort, request
 
 
 @app_views.route('/states/<state_id>/cities',
@@ -28,3 +28,23 @@ def get_city(city_id):
         return jsonify(city.to_dict())
     else:
         abort(404)
+
+
+@app_views.route('/states/<state_id>/cities',
+                 methods=['POST'], strict_slashes=False)
+def create_city(state_id):
+    """ Creates a City """
+    state = storage.get(State, state_id)
+    if not request.json:
+        return 'Not a JSON', 400
+    json_data = request.json
+    if 'name' not in json_data:
+        return 'Missing name', 400
+    if not state:
+        abort(404)
+
+    json_data['state_id'] = state_id
+    new_city = City(**json_data)
+    new_city.save()
+
+    return jsonify(new_city.to_dict()), 201
