bec64bb8ee5bba75048ed9683ae0b5d2f0f6b868
afarizap <afarizap@gmail.com>
2021-01-28 21:42:51 -0500

    fix import abort

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -3,7 +3,7 @@
 from api.v1.views import app_views
 from models import storage
 from models.state import State
-from flask import jsonify
+from flask import jsonify, abort
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
@@ -15,4 +15,13 @@ def list_states():
         dict_list.append(state.to_dict())
     return jsonify(dict_list)
 
+
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def get_state(state_id):
+    """ Retrieves a State object """
+    state = storage.get(State, state_id)
+    if state:
+        return jsonify(state.to_dict())
+    else:
+        abort(404)
 
