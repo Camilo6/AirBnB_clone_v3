ead33fe035ccdf14c7e8cb3fd2462525d95b9779
ChristianCampos-55 <1566@holbertonschool.com>
2021-01-29 20:00:32 -0500

    [FIX]Refact

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,3 @@
-#!/usr/bin/python3
-"""
-Initialize Blueprint views
-"""
 from flask import Blueprint
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
@@ -11,4 +7,5 @@ from api.v1.views.users import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
 from api.v1.views.places import *
-"""from api.v1.views.amenities import *"""
+from api.v1.views.amenities import *
+from api.v1.views.places_reviews import *
