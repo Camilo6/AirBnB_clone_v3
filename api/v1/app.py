e0432542b5eaede71d6ad84d04ae4f8ac16e0e3e
ChristianCampos-55 <1566@holbertonschool.com>
2021-01-29 20:09:11 -0500

    [ADD]Exec privs

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,19 +3,19 @@
 API file
 """
 
-from flask import Flask, Blueprint, make_response, jsonify
+from flask import Flask, Blueprint, render_template, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from flask_cors import CORS
-import os
+from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown_appcontext(exception):
+def teardown_appcontext(error):
     """close the storage on teardown"""
     storage.close()
 
@@ -27,6 +27,10 @@ def not_found(error):
 
 
 if __name__ == "__main__":
-    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=os.getenv('HBNB_API_PORT', 5000),
-            debug=True)
+    host = getenv('HBNB_API_HOST')
+    port = getenv('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = 5000
+    app.run(host=host, port=port, threaded=True)
